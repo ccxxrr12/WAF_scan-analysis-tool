337f7ebcbb13d056898a39b10e9b047b0861a0b0
SherryCHEN <1769108355@qq.com>
2025-11-03 21:24:58 +0800

    half save

--- Part1 waf_scanner/wafw00f/lib/asciiarts.py
+++ Part1 waf_scanner/wafw00f/lib/asciiarts.py
@@ -1,29 +1,39 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 '''
 Copyright (C) 2024, WAFW00F Developers.
 See the LICENSE file for copying permission.
 '''
 
-from dataclasses import dataclass
-from random import randint
+# 导入所需模块
+from dataclasses import dataclass  # 用于创建数据类
+from random import randint         # 用于生成随机整数
 
+# 导入项目版本信息
 from wafw00f import __version__
 
 
 @dataclass
 class Color:
-    """ANSI colors."""
+    """ANSI颜色代码类，用于终端彩色输出"""
+    # 白色
     W: str = '\033[1;97m'
+    # 黄色
     Y: str = '\033[1;93m'
+    # 绿色
     G: str = '\033[1;92m'
+    # 红色
     R: str = '\033[1;91m'
+    # 蓝色
     B: str = '\033[1;94m'
+    # 青色
     C: str = '\033[1;96m'
+    # 结束颜色代码
     E: str = '\033[0m'
 
     @classmethod
     def disable(cls):
-        """Disables all colors."""
+        """禁用所有颜色输出"""
         cls.W = ''
         cls.Y = ''
         cls.G = ''
@@ -34,8 +44,8 @@ class Color:
 
     @classmethod
     def unpack(cls):
-        """Unpacks and returns the color values.
-        Useful for brevity, e.g.:
+        """解包并返回颜色值
+        用于简化使用，例如：
         (W,Y,G,R,B,C,E) = Color.unpack()
         """
         return (
@@ -50,55 +60,18 @@ class Color:
 
 
 def randomArt():
-    # Colors for terminal
-
+    # 终端颜色设置
     (W,Y,G,R,B,C,E) = Color.unpack()
 
-    woof = '''
-                   '''+W+'''______
-                  '''+W+'''/      \\
-                 '''+W+'''(  Woof! )
-                  '''+W+r'''\  ____/                      '''+R+''')
-                  '''+W+''',,                           '''+R+''') ('''+Y+'''_
-             '''+Y+'''.-. '''+W+'''-    '''+G+'''_______                 '''+R+'''( '''+Y+'''|__|
-            '''+Y+'''()``; '''+G+'''|==|_______)                '''+R+'''.)'''+Y+'''|__|
-            '''+Y+'''/ ('        '''+G+r'''/|\                  '''+R+'''(  '''+Y+'''|__|
-        '''+Y+'''(  /  )       '''+G+r''' / | \                  '''+R+'''. '''+Y+'''|__|
-         '''+Y+r'''\(_)_))      '''+G+r'''/  |  \                   '''+Y+'''|__|'''+E+'''
-
-                    '''+C+'~ WAFW00F : '+B+'v'+__version__+''' ~'''+W+'''
-    The Web Application Firewall Fingerprinting Toolkit
-    '''+E
-
-    w00f = '''
-                '''+W+'''______
-               '''+W+'''/      \\
-              '''+W+'''(  W00f! )
-               '''+W+r'''\  ____/
-               '''+W+''',,    '''+G+'''__            '''+Y+'''404 Hack Not Found
-           '''+C+'''|`-.__   '''+G+'''/ /                     '''+R+''' __     __
-           '''+C+'''/"  _/  '''+G+'''/_/                       '''+R+r'''\ \   / /
-          '''+B+'''*===*    '''+G+'''/                          '''+R+r'''\ \_/ /  '''+Y+'''405 Not Allowed
-         '''+C+'''/     )__//                           '''+R+r'''\   /
-    '''+C+'''/|  /     /---`                        '''+Y+'''403 Forbidden
-    '''+C+r'''\\/`   \ |                                 '''+R+'''/ _ \\
-    '''+C+r'''`\    /_\\_              '''+Y+'''502 Bad Gateway  '''+R+r'''/ / \ \  '''+Y+'''500 Internal Error
-      '''+C+'''`_____``-`                             '''+R+r'''/_/   \_\\
-
-                        '''+C+'~ WAFW00F : '+B+'v'+__version__+''' ~'''+W+'''
-        The Web Application Firewall Fingerprinting Toolkit
-    '''+E
-
-    wo0f = r'''
-                 ?              ,.   (   .      )        .      "
-         __        ??          ("     )  )'     ,'        )  . (`     '`
-    (___()'`;   ???          .; )  ' (( (" )    ;(,     ((  (  ;)  "  )")
-    /,___ /`                 _"., ,._'_.,)_(..,( . )_  _' )_') (. _..( ' )
-    \\   \\                 |____|____|____|____|____|____|____|____|____|
-
-                                ~ WAFW00F : v'''+__version__+''' ~
-                    ~ Sniffing Web Application Firewalls since 2014 ~
+    #ASCII艺术字
+    woof = r'''
+        powered by SherryCHEN @SC
+        SCNOEK is NOt geEK
 '''
 
-    arts = [woof, w00f, wo0f]
+
+
+    # 将所有艺术字放入列表中
+    arts = [woof]
+    # 随机返回一个艺术字
     return arts[randint(0, len(arts)-1)]
